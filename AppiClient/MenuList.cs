52e51faa07067ed8486c272e03a708f724566b02
vostrenkoff <iseyth@gmail.com>
2019-08-15 15:37:37 +0300

    тоже тест

--- AppiClient/MenuList.cs
+++ AppiClient/MenuList.cs
@@ -7218,9 +7218,9 @@ namespace Client
             HideMenu();
             
             var menu = new Menu();
-            UiMenu = menu.Create("GPS", "~b~Меню вашего GPS");
+            UiMenu = menu.Create("GPS 123", "~b~Меню вашего GPSSS");
             
-            menu.AddMenuItem(UiMenu, "Важные места").Activated += (uimenu, item) =>
+            menu.AddMenuItem(UiMenu, "Важные местаыы").Activated += (uimenu, item) =>
             {
                 ShowGpsImportantMenu();
             };
